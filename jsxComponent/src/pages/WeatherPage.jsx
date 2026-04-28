d8832a628b7343f171948f2ebb43319c1a43f96b
Shaskia Putri Devi <shaskiapdv@gmail.com>
2026-04-28 17:14:33 +0700

    add snow (need fix)

--- jsxComponent/src/pages/WeatherPage.jsx
+++ jsxComponent/src/pages/WeatherPage.jsx
@@ -1,12 +1,27 @@
 import "../index.css";
+import PixelSnow from "../component/PixelSnow";
 
 function WeatherPage() {
   return (
     <div className="weather-container">
-      <h1>Weather App</h1>
-    
+        <div style={{ width: '100%', height: '600px', position: 'relative' }}>
+      <PixelSnow
+        color="#ffffff"
+        flakeSize={0.008}
+        minFlakeSize={1.25}
+        pixelResolution={200}
+        speed={1.35}
+        density={0.3}
+        direction={135}
+        brightness={1}
+        depthFade={8}
+        farPlane={20}
+        gamma={0.4545}
+        variant="square"
+      />
+      </div>
     </div>
-  )
+  );
 }
 
-export default WeatherPage
+export default WeatherPage;
